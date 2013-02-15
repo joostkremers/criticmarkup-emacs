46b60fa511694826003c6bf70d095a5e66351f1f
Joost Kremers <joostkremers@yahoo.com>
2013-02-15 08:11:56 +0100

    Unfill README and convert to plain text for inclusion in cm-mode.el

--- cm-mode.el
+++ cm-mode.el
@@ -33,40 +33,47 @@
 
 ;;; Commentary:
 
-;; `cm-mode` provides (rudimentary) support for [CriticMarkup](http://criticmarkup.com/)
+;; cm-mode is a minor mode that provides (rudimentary) support for
+;; CriticMarkup in Emacs.
 ;;
-;; CriticMarkup defines the following patterns for marking changes to a text:
-;; 
-;; Addition {++ ++}
-;; Deletion {-- --}
-;; Substitution {~~ ~> ~~}
-;; Comment {>> <<}
-;; Highlight {{ }}{>> <<}
-;; 
+;; CriticMarkup defines the following patterns for marking changes to a
+;; text:
+;;
+;; -   Addition {++ ++}
+;; -   Deletion {-- --}
+;; -   Substitution {~~ ~> ~~}
+;; -   Comment {>> <<}
+;; -   Highlight {{ }}{>> <<}
+;;
 ;; Note: additions are called insertions here, because it allows us to use
 ;; mnemonic key bindings.
 ;;
-;; Activating `cm-mode` adds the patterns above to font-lock-keywords and
-;; provides key bindings to insert the patterns above and thus mark one's
-;; changes to the text. The provided key bindings are:
+;; Activating cm-mode provides key bindings to insert the patterns above
+;; and thus mark one's changes to the text. The provided key bindings are:
 ;;
-;; C-c * i: insert text
-;; C-c * d: delete text
-;; C-c * s: substitute text
-;; C-c * c: insert a comment
-;; C-c * h: highlight text and insert a comment
+;; -   C-c * i: insert text
+;; -   C-c * d: delete text
+;; -   C-c * s: substitute text
+;; -   C-c * c: insert a comment
+;; -   C-c * h: highlight text and insert a comment
 ;;
 ;; The commands to delete, substitute and highlight text all operate on the
 ;; region. The commands for inserting and substituting text and for
 ;; inserting a comment (which includes the command to highlight text) all
-;; put the cursor at the correct position, so you can start typing right away.
+;; put the cursor at the correct position, so you can start typing right
+;; away.
+;;
+;; cm-mode also adds the markup patterns defined by CriticMarkup to
+;; font-lock-keywords and provides customisable faces to highlight them.
+;; The customisation group is called criticmarkup.
 ;;
 ;; TODO:
 ;;
-;; - Commands to accept or reject the change at point (C-c * a and C-c * r)
-;; - Command to accept or reject all changes interactively (C-c * A)
-;; - Mouse support
-
+;; -   Commands to accept or reject the change at point (C-c * a and
+;;     C-c * r)
+;; -   Command to accept or reject all changes interactively (C-c * A)
+;; -   Mouse support
+;;
 ;;; Code:
 
 (defvar cm-insertion-regexp "\\(?:{\\+\\+.*?\\+\\+}\\)"
