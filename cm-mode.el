7366c47102b52494010eaa800facf3d05afa00cb
Joost Kremers <joostkremers@fastmail.fm>
2013-02-28 02:48:16 +0100

    Show buffer's author in the modeline.

--- cm-mode.el
+++ cm-mode.el
@@ -209,7 +209,7 @@ it is added automatically."
 ;;;###autoload
 (define-minor-mode cm-mode
   "Minor mode for CriticMarkup."
-  :init-value nil :lighter (:eval (concat " cm" (if cm-follow-changes "*"))) :global nil
+  :init-value nil :lighter (:eval (concat " cm" (if cm-author (concat "@" cm-author)) (if cm-follow-changes "*"))) :global nil
   (cond
    (cm-mode                             ; cm-mode is turned on
     (font-lock-add-keywords nil `((,cm-addition-regexp . cm-addition-face)
