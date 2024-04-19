b32ecf8995537d6d0495fac6560cf49a8ffa14fc
Joost Kremers <joostkremers@fastmail.com>
2024-04-19 09:18:20 +0200

    Update version number.

--- cm-mode.el
+++ cm-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Joost Kremers <joostkremers@fastmail.fm>
 ;; Maintainer: Joost Kremers <joostkremers@fastmail.fm>
 ;; Created: 14 Feb 2013
-;; Version: 1.6
+;; Version: 1.7
 ;; Keywords: text, markdown
 ;; Package-Requires: ((cl-lib "0.5"))
 
